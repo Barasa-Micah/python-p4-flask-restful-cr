4c0f116ae276e5d07bc6e49178f9d3bec42e39cc
Barasa-Micah <barasamica@gmail.com>
2023-09-23 13:42:07 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, Index
 
 from models import db, Newsletter
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR']= True
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -16,8 +17,65 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
-    pass
+
+class index(Resource):
+    
+    def get(self):
+        response_dict ={
+            'index': 'Welcome to the NewssletterRESTful API',
+        }
+
+        response = make_response(
+            jsonify(response_dict)
+        )
+
+        return response
+    
+api.add_response(Index, '/')
+
+class Newsletters(Resource):
+
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200
+        )
+
+        return response
+    
+    def post(self):
+
+        new_record = Newsletter(
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+        
+        response=make_response(
+            jsonify(response_dict),
+            201
+        )
+
+        return response
+    
+api.add_resource(Newsletter, '/newsletters')
+
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict=- Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response=make_response(
+            api.add_resource(NewsletterByID, '/newsletter/<int:id>')
+            
+        )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
